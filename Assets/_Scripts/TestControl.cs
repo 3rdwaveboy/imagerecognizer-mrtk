1e49612702e4fff1fb77df4f182658ca3cc85710
stefan.wasserbauer <stefan.wasserbauer@dynatrace.com>
2018-12-15 22:31:27 +0100

    Fixed text reference, create mixed reality service

--- Assets/_Scripts/TestControl.cs
+++ Assets/_Scripts/TestControl.cs
@@ -1,26 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Microsoft.MixedReality.Toolkit.Core.Services;
+﻿using Microsoft.MixedReality.Toolkit.Core.Services;
+using Microsoft.MixedReality.Toolkit.Core.Utilities;
 using UnityEngine;
-using _Scripts;
 
-public class test : MonoBehaviour {
+namespace _Scripts {
+    public class TestControl : MonoBehaviour {
     
-    public GameObject OutputText;
-    private TextMesh OutputTextMesh;
+        private TextMesh _outputTextMesh;
     
-    // Use this for initialization
-    void Start() {
-        OutputTextMesh = OutputText.GetComponent<TextMesh>();
-        OutputTextMesh.text = string.Empty;
-        MixedRealityToolkit.Instance.GetService<ImageRecognitionService>().OnRecognition += OnRecognition;
-    }
+        // Use this for initialization
+        void Start() {
+            _outputTextMesh = CameraCache.Main.GetComponentInChildren<TextMesh>();
+            _outputTextMesh.text = "Start Recognizing";
+            MixedRealityToolkit.Instance.GetService<ImageRecognitionService>().OnRecognition += OnRecognition;
+        }
 
-    private void OnRecognition(RecognitionResult obj) {
-        Debug.LogWarning(obj.Message);
-        OutputTextMesh.text = obj.Message;
-    }
+        private void OnRecognition(RecognitionResult obj) {
+            Debug.LogWarning(obj.Message);
+            _outputTextMesh.text = obj.Message;
+        }
 
-    // Update is called once per frame
-    void Update() { }
+    }
 }
